d8ed7332da02c3309aeef94e3742cdf653bb3f6b
Yang Meng <mengyang0529@gmail.com>
2024-12-11 15:16:36 +0900

    clear up measure

--- timeline/vertical/measure.py
+++ timeline/vertical/measure.py
@@ -10,54 +10,22 @@ class MeasureLayout:
             w, h = SUB_TOPIC_BOX_WIDTH, SUB_TOPIC_BOX_HEIGHT
         else:
             w, h = DETAIL_BOX_WIDTH, DETAIL_BOX_HEIGHT
-
-        if node.level < 2:
-            # Level=0 or level=1 vertically stack child nodes
-            if node.level == 0:
-                # Vertically arrange level=1 child nodes
-                if node.children:
-                    max_child_width = w
-                    total_height = h
-                    for i, c in enumerate(node.children):
-                        max_child_width = max(max_child_width, c.required_width)
-                        total_height += c.required_height + SPACING
-                        c.is_direction = (i % 2 == 0)
-                    total_height -= SPACING  # Remove the last extra SPACING
-                    node.required_width = max_child_width
-                    node.required_height = total_height
-                else:
-                    node.required_width = w
-                    node.required_height = h
-            elif node.level == 1:
-                # Vertically stack level=2 child nodes
-                if node.children:
-                    max_child_width = 0
-                    total_child_height = 0
-                    for c in node.children:
-                        max_child_width = max(max_child_width, c.required_width)
-                        total_child_height += c.required_height + DETAIL_SPACING
-                        c.is_direction = node.is_direction
-                    total_child_height -= DETAIL_SPACING
-                    node.required_width = max(w, max_child_width)
-                    node.required_height = h + total_child_height
-                else:
-                    node.required_width = w
-                    node.required_height = h
-        else:
-            # Level=2 and above: vertically stack child nodes
+            
+        node.required_width = w
+        node.required_height = h
+        
+        if node.level == 0:
             if node.children:
-                max_child_width = w
-                total_height = h
+                for i, c in enumerate(node.children):
+                    c.is_direction = (i % 2 == 0)  # Even indexed child nodes are above, odd indexed are below
+        elif node.level == 1:
+            if node.children:
                 for c in node.children:
-                    max_child_width = max(max_child_width, c.required_width)
-                    total_height += c.required_height + DETAIL_SPACING
-                    c.is_direction = node.is_direction
-                total_height -= DETAIL_SPACING
-                node.required_width = max_child_width
-                node.required_height = total_height
-            else:
-                node.required_width = w
-                node.required_height = h
+                    c.is_direction = node.is_direction  # Child nodes inherit parent's is_direction attribute
+        else:
+            if node.children:
+                for c in node.children:
+                    c.is_direction = node.is_direction  # Child nodes inherit parent's is_direction attribute
 
         for c in node.children:
             c.parent = node
